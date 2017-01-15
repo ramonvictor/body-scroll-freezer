4a8365992e15265b0a03134d9aafb1eadafe6bf6
Ramon Bezerra <ramon.bezerra@booking.com>
2017-01-15 22:01:00 +0100

    change tabs indentation to two spaces

--- body-scroll-freezer.js
+++ body-scroll-freezer.js
@@ -1,126 +1,126 @@
 (function() {
-	'use strict';
-	/*
-	 * Dependency-free module to freeze body scroll when opening modal components.
-	 * @module body-scroll-freezer
-	 */
-	var API = {};
-	var measureScrollClass = 'js-scrollbar-measure';
-	var body = document.body;
-	var scrollWidth;
-	var isFroozen = false;
-	var supportsEventListener = ('addEventListener' in Element.prototype);
+  'use strict';
+  /*
+   * Dependency-free module to freeze body scroll when opening modal components.
+   * @module body-scroll-freezer
+   */
+  var API = {};
+  var measureScrollClass = 'js-scrollbar-measure';
+  var body = document.body;
+  var scrollWidth;
+  var isFroozen = false;
+  var supportsEventListener = ('addEventListener' in Element.prototype);
 
-	/**
-	 * Init module by getting browser scroll width.
-	 * @public
-	 * @return {Number} Browser scroll bar width
-	 */
-	function init() {
-		/* jshint validthis:true */
-		return (this.scrollWidth = scrollWidth = getScrollWidth());
-	}
+  /**
+   * Init module by getting browser scroll width.
+   * @public
+   * @return {Number} Browser scroll bar width
+   */
+  function init() {
+    /* jshint validthis:true */
+    return (this.scrollWidth = scrollWidth = getScrollWidth());
+  }
 
-	/**
-	 * Freeze body scroll.
-	 * @public
-	 */
-	function freeze() {
-		if (!scrollWidth) {
-			return;
-		}
+  /**
+   * Freeze body scroll.
+   * @public
+   */
+  function freeze() {
+    if (!scrollWidth) {
+      return;
+    }
 
-		body.style.overflow = 'hidden';
-		if (windowHasScroll()) {
-			body.style.paddingRight = scrollWidth + 'px';
-		}
+    body.style.overflow = 'hidden';
+    if (windowHasScroll()) {
+      body.style.paddingRight = scrollWidth + 'px';
+    }
 
-		if (supportsEventListener) {
-			toggleResizeListener();
-		}
-	}
+    if (supportsEventListener) {
+      toggleResizeListener();
+    }
+  }
 
-	/**
-	 * Unfreeze body scroll.
-	 * @public
-	 */
-	function unfreeze() {
-		if (!scrollWidth) {
-			return;
-		}
+  /**
+   * Unfreeze body scroll.
+   * @public
+   */
+  function unfreeze() {
+    if (!scrollWidth) {
+      return;
+    }
 
-		body.style.overflow = '';
-		body.style.paddingRight = '';
+    body.style.overflow = '';
+    body.style.paddingRight = '';
 
-		if (supportsEventListener) {
-			toggleResizeListener();
-		}
-	}
+    if (supportsEventListener) {
+      toggleResizeListener();
+    }
+  }
 
-	/**
-	 * Append/remove `div.js-scrollbar-measure` just to measure scroll bar width.
-	 * @private
-	 * @return {Number} Browser scroll bar width
-	 */
-	function getScrollWidth() {
-		var div = document.createElement('div');
-		var scrollBarWidth;
+  /**
+   * Append/remove `div.js-scrollbar-measure` just to measure scroll bar width.
+   * @private
+   * @return {Number} Browser scroll bar width
+   */
+  function getScrollWidth() {
+    var div = document.createElement('div');
+    var scrollBarWidth;
 
-		div.className = measureScrollClass;
-		body.appendChild(div);
-		scrollBarWidth = div.offsetWidth - div.clientWidth;
-		body.removeChild(div);
+    div.className = measureScrollClass;
+    body.appendChild(div);
+    scrollBarWidth = div.offsetWidth - div.clientWidth;
+    body.removeChild(div);
 
-		return scrollBarWidth;
-	}
+    return scrollBarWidth;
+  }
 
-	/**
-	 * Check whether window scroll is visible or not.
-	 * @private
-	 * @return {Boolean}
-	 */
-	function windowHasScroll() {
-		return (
-			body.scrollHeight > document.documentElement.clientHeight
-		);
-	}
+  /**
+   * Check whether window scroll is visible or not.
+   * @private
+   * @return {Boolean}
+   */
+  function windowHasScroll() {
+    return (
+      body.scrollHeight > document.documentElement.clientHeight
+    );
+  }
 
-	/**
-	 * Switch resize listener on/off depending on freeze state.
-	 * @private
-	 */
-	function toggleResizeListener() {
-		isFroozen = (isFroozen === true ? false : true);
+  /**
+   * Switch resize listener on/off depending on freeze state.
+   * @private
+   */
+  function toggleResizeListener() {
+    isFroozen = (isFroozen === true ? false : true);
 
-		if (isFroozen) {
-			window.addEventListener('resize', onWindowResize, false);
-		} else {
-			window.removeEventListener('resize', onWindowResize, false);
-		}
-	}
+    if (isFroozen) {
+      window.addEventListener('resize', onWindowResize, false);
+    } else {
+      window.removeEventListener('resize', onWindowResize, false);
+    }
+  }
 
-	/**
-	 * Update body padding-right depending on window scroll visibility.
-	 * @private
-	 */
-	function onWindowResize() {
-		if (windowHasScroll()) {
-			body.style.paddingRight = scrollWidth + 'px';
-		} else {
-			body.style.paddingRight = '';
-		}
-	}
+  /**
+   * Update body padding-right depending on window scroll visibility.
+   * @private
+   */
+  function onWindowResize() {
+    if (windowHasScroll()) {
+      body.style.paddingRight = scrollWidth + 'px';
+    } else {
+      body.style.paddingRight = '';
+    }
+  }
 
-	/**
-	 * Public api.
-	 */
-	API.init = init;
-	API.freeze = freeze;
-	API.unfreeze = unfreeze;
+  /**
+   * Public api.
+   */
+  API.init = init;
+  API.freeze = freeze;
+  API.unfreeze = unfreeze;
 
-	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-		module.exports = API;
-	} else if (typeof window !== 'undefined') {
-		window.bodyScrollFreezer = API;
-	}
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = API;
+  } else if (typeof window !== 'undefined') {
+    window.bodyScrollFreezer = API;
+  }
 })();
