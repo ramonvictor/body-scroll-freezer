843a364b48cbe6c754c33c09c0699ff8d7dbaf37
Ramon Bezerra <ramon.bezerra@booking.com>
2017-01-16 21:48:23 +0100

    make package ready for npm publish

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var header = require('gulp-header');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var target = {
@@ -9,13 +10,26 @@ var target = {
 };
 
 gulp.task('js-uglify', function() {
+	var pkg = require('./package.json');
+	var banner = [
+	  '/**',
+	  ' * <%= pkg.name %> - <%= pkg.description %>',
+	  ' *',
+	  ' * @version v<%= pkg.version %>',
+	  ' * @link <%= pkg.homepage %>',
+	  ' * @author <%= pkg.author %>',
+	  ' * @license <%= pkg.license %>',
+	  ' */',
+	  ''].join('\n');
+
 	gulp.src(target.js_concat_src)
-	.pipe(uglify())
-	.pipe(rename(function(dir, base, ext){
-		var trunc = base.split('.')[0];
-		return trunc + '.min' + ext;
-	}))
-	.pipe(gulp.dest(target.js_dest));
+		.pipe(uglify())
+		.pipe(header(banner, {pkg: pkg}))
+		.pipe(rename(function(dir, base, ext){
+			var trunc = base.split('.')[0];
+			return trunc + '.min' + ext;
+		}))
+		.pipe(gulp.dest(target.js_dest));
 });
 
 gulp.task('js-lint', function() {
